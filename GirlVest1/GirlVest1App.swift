4a086354db1aa5cdff55f81e7b30d0b473612a3a
Krisha Dani <danikrisha@gmail.com>
2023-08-15 11:42:15 -0400

    Initial Commit

---
+++ GirlVest1/GirlVest1App.swift
@@ -0,0 +1,17 @@
+//
+//  GirlVest1App.swift
+//  GirlVest1
+//
+//  Created by Scholar on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct GirlVest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
